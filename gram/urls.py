51057fc6a1ab4fd419259a6ba81fd6f47a76a7ea
umunadine <umutesinad3@gmail.com>
2021-01-22 13:55:04 +0200

    adding account route

--- gram/urls.py
+++ gram/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'',include('gram.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.LoginView.as_view(template_name='registration/login.html'), {"next_page": '/'}),
+    url(r'^logout/$', views.LogoutView.as_view(template_name='registration/logout.html'), {"next_page": '/'}),
 ]

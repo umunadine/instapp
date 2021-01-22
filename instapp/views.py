2c5995d687571faee42110c2c83723240ba62fc4
umunadine <umutesinad3@gmail.com>
2021-01-22 13:42:10 +0200

    creating route

--- instapp/views.py
+++ instapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+import datetime as dt
+from .models import Post,Comment,Follow,Profile
+from django.contrib.auth.decorators import login_required
+from .forms import NewPostForm, NewCommentForm, AddProfileForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+def signup(request):
+        if request.method == 'POST':
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.save()
+                new_profile = Profile(user=user)
+        else:
+            form = SignupForm()
+    return render(request, 'registration/registration_form.html',{'form':form})

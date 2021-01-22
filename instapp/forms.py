f9d9d19468d5c4210d8de81736256c6f0ccf04d4
umunadine <umutesinad3@gmail.com>
2021-01-22 20:51:59 +0200

    post model

--- instapp/forms.py
+++ instapp/forms.py
@@ -5,3 +5,8 @@ class AddProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user', 'date_posted','profile','like']
